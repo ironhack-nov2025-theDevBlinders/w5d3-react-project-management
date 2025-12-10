76e188aab2559a6b02448bfcaa521af14a2d7278
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-12-10 10:56:07 +0100

    feat: implement list of projects

--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -1,9 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import Loader from "../components/Loader";
+
 
 function ProjectListPage() {
+
+    const [projects, setProjects] = useState(null)
+
+    useEffect(() => {
+        axios.get("https://project-management-api.edu.ironhack.com/projects")
+            .then((response) => {
+                setProjects(response.data)
+            })
+            .catch(e => console.log("Error getting projects from the API", e))
+    }, [])
+
+    if (projects === null) {
+        return <Loader />
+    }
 
     return (
         <div>
             <h1>Project List Page</h1>
+            <h2>Number of projects: {projects.length}</h2>
+
+            {projects.map((projectObj) => {
+                return (
+                    <div className="card" key={projectObj.id}>
+                        <h3>{projectObj.title}</h3>
+                        <p>{projectObj.description}</p>
+                    </div>
+                )
+            })}
         </div>
     );
 }
