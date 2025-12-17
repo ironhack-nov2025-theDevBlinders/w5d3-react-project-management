3ca0bdd83db1a3cae2b7fdf5a3a57afb393438a7
Luis Junco <38411763+luisjunco@users.noreply.github.com>
2025-12-17 10:13:41 +0100

    fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 
-import Navbar from "./Navbar"
+import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import ProjectListPage from "./pages/ProjectListPage"
 import CreateProjectPage from "./pages/CreateProjectPage"
